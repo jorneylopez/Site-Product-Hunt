e815640a7347d4025c2ef68b67d8f2da6d9a97a9
Jorney <jorney95@live.com>
2021-07-12 18:55:20 -0600

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layouts/Layout';
+export default function Home() {
+  return (
+    <div >
+      <Layout>
+        <h1>Inicio</h1>
+      </Layout>
+    </div>
+  )
+}
